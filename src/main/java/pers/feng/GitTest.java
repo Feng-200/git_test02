01e364ee5a8e9aed40208f99fdc202edefc3c914
Feng <1318924552@qq.com>
2021-05-04 21:21:05 +0800

    push 01 commit

--- src/main/java/pers/feng/GitTest.java
+++ src/main/java/pers/feng/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("master 01");
         System.out.println("hot-fix 01");
+        System.out.println("push test!");
     }
 }
