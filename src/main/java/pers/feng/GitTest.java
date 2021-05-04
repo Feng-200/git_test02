5b52ede25a23c6264d8cc3b1e2cf69ad2970ede9
Feng <1318924552@qq.com>
2021-05-04 20:50:57 +0800

    hot-fix 01 commit

--- src/main/java/pers/feng/GitTest.java
+++ src/main/java/pers/feng/GitTest.java
@@ -7,5 +7,6 @@ package pers.feng;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("hot-fix 01");
     }
 }
