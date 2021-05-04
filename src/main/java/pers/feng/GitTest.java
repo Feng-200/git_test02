56179816e49b830a2764dff34a2f0b21c89b7f86
Feng <1318924552@qq.com>
2021-05-04 20:49:54 +0800

    02 commit

---
+++ src/main/java/pers/feng/GitTest.java
@@ -0,0 +1,11 @@
+package pers.feng;
+
+/**
+ * @author Feng
+ * @date 2021/5/4 20:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
