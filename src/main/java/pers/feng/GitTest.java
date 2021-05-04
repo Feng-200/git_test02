f16cb97c4aecdfa652adb934210bcf39c5ecff4e
Feng-404 <77630970+Feng-404@users.noreply.github.com>
2021-05-04 21:23:22 +0800

    pull commit!

--- src/main/java/pers/feng/GitTest.java
+++ src/main/java/pers/feng/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("master 01");
         System.out.println("hot-fix 01");
         System.out.println("push test!");
-        System.out.println("pull test!");
+        System.out.println("pull01 test!");
     }
 }
