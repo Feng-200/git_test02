aba600fa4937fa48f9b809c09f8dc830404857d5
Feng-404 <77630970+Feng-404@users.noreply.github.com>
2021-05-04 21:22:44 +0800

    Update GitTest.java

--- src/main/java/pers/feng/GitTest.java
+++ src/main/java/pers/feng/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master 01");
         System.out.println("hot-fix 01");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
